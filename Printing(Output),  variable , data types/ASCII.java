6d59f08d3e8d18574143dec3d64a8f113a407323
PRIYUSH KHOBRAGADE <124695270+Priyush02K@users.noreply.github.com>
2023-06-13 12:05:55 +0530

    Add files via upload

---
+++ Printing(Output),  variable , data types/ASCII.java
@@ -0,0 +1,8 @@
+public class ASCII {
+         public static void main(String[] args) {
+
+            int X='U';
+            System.out.println("The ASCll Value of U is : " +X);
+         }
+    
+}
